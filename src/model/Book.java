db93264ff6de2f602bd9c86a92751ed21b944962
Joodar <joodartynystanov@gmail.com>
2026-06-23 01:10:01 +0600

    Добавил модели для книги и починил баг

--- src/model/Book.java
+++ src/model/Book.java
@@ -7,5 +7,36 @@ public class Book {
     private BookStatus status;
     private int idEmployee;
 
+    public Book(int id, String author,  String image, BookStatus status, int idEmployee) {
+        this.id = id;
+        this.author = author;
+        this.image = image;
+        this.status = status;
+        this.idEmployee = idEmployee;
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public int getIdEmployee() {
+        return idEmployee;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
 }
