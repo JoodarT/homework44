062a9bdcbdeb1df10d33378c51da038d5efb0915
Joodar <joodartynystanov@gmail.com>
2026-06-23 14:16:20 +0600

    add Repository

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,7 +1,40 @@
 package model;
 
+import java.util.List;
+
 public class Employee {
     private int idEmployee;
     private String fullname;
     private String firstname;
+    private List<Integer>currentBooks;
+    private  List<Integer>historyBooks;
+
+    public Employee(int idEmployee, String fullname, String firstname,
+                    List<Integer> currentBooks, List<Integer> historyBooks) {
+        this.idEmployee = idEmployee;
+        this.fullname = fullname;
+        this.firstname = firstname;
+        this.currentBooks = currentBooks;
+        this.historyBooks = historyBooks;
+    }
+
+    public int getIdEmployee() {
+        return idEmployee;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public List<Integer> getCurrentBooks() {
+        return currentBooks;
+    }
+
+    public List<Integer> getHistoryBooks() {
+        return historyBooks;
+    }
 }
---
+++ src/repository/Repository.java
@@ -0,0 +1,47 @@
+package repository;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import model.Book;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repository {
+    private String dataDir = "data";
+    private String fileName = "books.json";
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private List<Book> books = new ArrayList<>();
+
+    public Repository(){
+        loadBooks();
+    }
+
+    private void loadBooks(){
+        Path path = Paths.get(dataDir, fileName);
+        if (!Files.exists(path)){
+            System.out.printf("Файл   " + fileName + "не найден, инициализирован пустой список.");
+
+            this.books = new ArrayList<>();
+
+            return;
+        }
+
+        try(Reader reader = Files.newBufferedReader(path)){
+
+            books = gson.fromJson(reader, new TypeToken<List<Book>>(){}.getType());
+
+        }catch (IOException e){
+            System.out.printf("Ошибка при чтении файла книг" + e.getMessage());
+        }
+    }
+
+
+}
